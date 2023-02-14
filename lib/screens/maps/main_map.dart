1fd06a840198f9151bfd13b3cf6ab5f97c6360d2
White-Patrasche <5338095@gmail.com>
2023-02-14 12:11:47 +0900

    feat: map view

--- lib/screens/maps/main_map.dart
+++ lib/screens/maps/main_map.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:garbage_collector/screens/screens.dart';
 import 'package:garbage_collector/states/states.dart';
 import 'package:garbage_collector/styles/color.dart';
-import 'package:garbage_collector/widgets/widgets.dart';
+import 'package:garbage_collector/utils/utils.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MainMap extends StatefulWidget {
   const MainMap({super.key});
@@ -19,28 +21,6 @@ class _MainMap extends State<MainMap> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(
-        title: 'Garbage Collector',
-        allowBackButton: false,
-        actions: [
-          GestureDetector(
-            onTap: () async {
-              Get.to(() => const SettingScreen());
-            },
-            behavior: HitTestBehavior.translucent,
-            child: Container(
-              height: 60,
-              padding: const EdgeInsets.only(right: 20),
-              child: const Center(
-                child: Icon(
-                  Icons.person,
-                  color: ColorSystem.primary,
-                ),
-              ),
-            ),
-          )
-        ],
-      ),
       body: Stack(
         children: [
           GoogleMap(
@@ -49,93 +29,105 @@ class _MainMap extends State<MainMap> {
                 _globalStates.mapController = controller;
               });
             },
-            myLocationButtonEnabled: true,
-            myLocationEnabled: true,
             initialCameraPosition:
                 CameraPosition(target: _globalStates.latlng, zoom: 16),
-            markers: {
-              Marker(
-                  icon: BitmapDescriptor.defaultMarker,
-                  markerId: MarkerId('1234'),
-                  position: _globalStates.latlng,
-                  onTap: () {
-                    Get.dialog(
-                      Dialog(
-                          child: Container(
-                        width: 150,
-                        height: 100,
-                        color: Colors.white,
-                        child: Row(
-                          children: [
-                            Container(
-                              height: 100,
-                              width: 100,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(15),
-                                  color: Colors.blueAccent),
-                            ),
-                            Flexible(
-                              child: Column(children: [
-                                Text('선유기지'),
-                                Text('이곳에서 Garbage Collection을 3회 진행했습니다.'),
-                                Row(
-                                  children: [
-                                    Icon(
-                                      Icons.location_on_rounded,
-                                      color: Colors.grey,
-                                    ),
-                                    Text('현재 위치에서 300m')
-                                  ],
-                                ),
-                              ]),
-                            ),
-                          ],
-                        ),
-                      )),
-                    );
-                  })
-            },
+            myLocationButtonEnabled: false,
+            markers: _globalStates.markerList,
           ),
-          // Positioned(
-          //     right: 10,
-          //     bottom: 70,
-          //     child: GestureDetector(
-          //       onTap: () async {
-          //         await _globalStates.mapController
-          //             .animateCamera(CameraUpdate.zoomIn());
-          //       },
-          //       child: Container(
-          //           height: 40,
-          //           width: 40,
-          //           alignment: Alignment.center,
-          //           decoration: BoxDecoration(
-          //               color: ColorSystem.primary,
-          //               borderRadius: BorderRadius.circular(5)),
-          //           child: const Text(
-          //             '+',
-          //             style: TextStyle(color: Colors.white, fontSize: 21),
-          //           )),
-          //     )),
-          // Positioned(
-          //     right: 10,
-          //     bottom: 10,
-          //     child: GestureDetector(
-          //       onTap: () async {
-          //         await _globalStates.mapController
-          //             .animateCamera(CameraUpdate.zoomOut());
-          //       },
-          //       child: Container(
-          //           height: 40,
-          //           width: 40,
-          //           alignment: Alignment.center,
-          //           decoration: BoxDecoration(
-          //               color: ColorSystem.primary,
-          //               borderRadius: BorderRadius.circular(5)),
-          //           child: const Text(
-          //             '-',
-          //             style: TextStyle(color: Colors.white, fontSize: 21),
-          //           )),
-          //     )),
+          Positioned(
+              right: 10,
+              bottom: 90,
+              child: GestureDetector(
+                onTap: () async {
+                  _globalStates.loadMarkers();
+                  final location = await Location.instance.getLocation();
+                  if (location.latitude == null || location.longitude == null) {
+                    showToast('현재 위치를 읽는 데 오류가 발생했습니다.');
+                    return;
+                  }
+                  await _globalStates.mapController.animateCamera(
+                      CameraUpdate.newLatLngZoom(
+                          LatLng(location.latitude!, location.longitude!), 17));
+                },
+                child: Container(
+                    height: 40,
+                    width: 40,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: ColorSystem.primary,
+                        borderRadius: BorderRadius.circular(5),
+                        border:
+                            Border.all(color: Colors.white.withOpacity(0.5))),
+                    child: const Icon(
+                      Icons.my_location_rounded,
+                      color: Colors.white,
+                      size: 24,
+                    )),
+              )),
+          Positioned(
+              right: 10,
+              bottom: 50,
+              child: GestureDetector(
+                onTap: () async {
+                  await _globalStates.mapController
+                      .animateCamera(CameraUpdate.zoomIn());
+                },
+                child: Container(
+                    height: 40,
+                    width: 40,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: ColorSystem.primary,
+                        borderRadius: BorderRadius.circular(5),
+                        border:
+                            Border.all(color: Colors.white.withOpacity(0.5))),
+                    child: const Text(
+                      '+',
+                      style: TextStyle(color: Colors.white, fontSize: 21),
+                    )),
+              )),
+          Positioned(
+            right: 10,
+            bottom: 10,
+            child: GestureDetector(
+                onTap: () async {
+                  await _globalStates.mapController
+                      .animateCamera(CameraUpdate.zoomOut());
+                },
+                child: Container(
+                    height: 40,
+                    width: 40,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: ColorSystem.primary,
+                        borderRadius: BorderRadius.circular(5),
+                        border:
+                            Border.all(color: Colors.white.withOpacity(0.5))),
+                    child: const Text(
+                      '-',
+                      style: TextStyle(color: Colors.white, fontSize: 21),
+                    ))),
+          ),
+          Positioned(
+            top: 10,
+            right: 10,
+            child: GestureDetector(
+              onTap: () {
+                Get.to(() => const SettingScreen());
+              },
+              child: Container(
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: const Icon(
+                    Icons.settings,
+                    color: ColorSystem.primary,
+                    size: 24,
+                  )),
+            ),
+          ),
           Positioned.fill(
             bottom: 10,
             child: Align(
@@ -144,16 +136,10 @@ class _MainMap extends State<MainMap> {
                 onTap: () {
                   Get.to(() => const CameraScreen());
                 },
-                child: Container(
-                    padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                    decoration: BoxDecoration(
-                      color: ColorSystem.primary,
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    child: const Icon(
-                      Icons.change_circle_rounded,
-                      color: Colors.white,
-                    )),
+                child: SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: SvgPicture.asset('assets/icons/camera_example.svg')),
               ),
             ),
           ),
