7ccc5be8daefa98c2c79991b796346faa3c6ad44
White-Patrasche <5338095@gmail.com>
2023-02-22 19:03:39 +0900

    fix: firebase init error for default profile

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,9 @@ import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  //Firebase init
+  await Firebase.initializeApp(
+      name: 'Garbage-Collector',
+      options: DefaultFirebaseOptions.currentPlatform);
   runApp(const GarbageCollector());
 }
 
