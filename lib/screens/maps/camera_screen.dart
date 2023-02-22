fd05f1ff409e4e224702012da5da5866e7927aae
White-Patrasche <5338095@gmail.com>
2023-02-22 19:05:42 +0900

    feat: camera screen without classify

--- lib/screens/maps/camera_screen.dart
+++ lib/screens/maps/camera_screen.dart
@@ -1,7 +1,13 @@
 import 'dart:io';
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:garbage_collector/screens/screens.dart';
+import 'package:get/get.dart';
+import 'package:garbage_collector/styles/styles.dart';
+import 'package:garbage_collector/states/states.dart';
+import 'package:garbage_collector/widgets/widgets.dart';
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({super.key});
@@ -25,8 +31,13 @@ class _CameraScreen extends State<CameraScreen> {
 
   Future<void> _initCamera() async {
     final cameras = await availableCameras();
-    _cameraController =
-        CameraController(cameras[cameraIndex], ResolutionPreset.veryHigh);
+    _cameraController = CameraController(
+      cameras[cameraIndex],
+      ResolutionPreset.high,
+      enableAudio: false,
+      imageFormatGroup: ImageFormatGroup.jpeg,
+    );
+    _cameraController?.setFlashMode(FlashMode.off);
     _initCameraControllerFuture = _cameraController!.initialize().then((value) {
       setState(() {});
     });
@@ -34,167 +45,120 @@ class _CameraScreen extends State<CameraScreen> {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: isCapture
-          ? Column(
+          ? Stack(
               children: [
                 /// 촬영 된 이미지 출력
-                Flexible(
-                  flex: 3,
-                  fit: FlexFit.tight,
-                  child: Container(
-                    width: size.width,
-                    height: size.width,
-                    child: ClipRect(
-                      child: FittedBox(
-                        fit: BoxFit.fitWidth,
-                        child: SizedBox(
-                          width: size.width,
-                          child: AspectRatio(
-                            aspectRatio:
-                                1 / _cameraController!.value.aspectRatio,
-                            child: Container(
-                              width: size.width,
-                              decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                image: MemoryImage(
-                                    captureImage!.readAsBytesSync()),
-                              )),
-                            ),
-                          ),
-                        ),
+                Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      fit: BoxFit.cover,
+                      opacity: 0.4,
+                      colorFilter: const ColorFilter.mode(
+                        Colors.black,
+                        BlendMode.dstATop,
                       ),
+                      image: MemoryImage(captureImage!.readAsBytesSync()),
                     ),
                   ),
                 ),
-                Flexible(
-                  flex: 2,
-                  fit: FlexFit.tight,
-                  child: InkWell(
-                    onTap: () {
-                      /// 재촬영 선택시 카메라 삭제 및 상태 변경
-                      captureImage!.delete();
-                      captureImage = null;
-                      setState(() {
-                        isCapture = false;
-                      });
-                    },
-                    child: Container(
-                      width: double.infinity,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(
-                            Icons.arrow_back,
-                            color: Colors.white,
-                          ),
-                          SizedBox(height: 16.0),
-                          Text(
-                            "다시 찍기",
-                            style: TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                const Center(child: TrashScreen()),
+
+                const Positioned(
+                  left: 10,
+                  top: 20,
+                  child: GoingBackButton(),
                 ),
+
+                // Flexible(
+                //   flex: 2,
+                //   fit: FlexFit.tight,
+                //   child: InkWell(
+                //     onTap: () {
+                //       /// 재촬영 선택시 카메라 삭제 및 상태 변경
+                //       captureImage!.delete();
+                //       captureImage = null;
+                //       setState(() {
+                //         isCapture = false;
+                //       });
+                //     },
+                //     child: SizedBox(
+                //       width: double.infinity,
+                //       child: Column(
+                //         mainAxisAlignment: MainAxisAlignment.center,
+                //         children: const [
+                //           Icon(
+                //             Icons.arrow_back,
+                //             color: Colors.white,
+                //           ),
+                //           SizedBox(height: 16.0),
+                //           Text(
+                //             "다시 찍기",
+                //             style: TextStyle(
+                //                 fontSize: 16.0,
+                //                 color: Colors.white,
+                //                 fontWeight: FontWeight.bold),
+                //           ),
+                //         ],
+                //       ),
+                //     ),
+                //   ),
+                // ),
               ],
             )
-          : Column(
+          : Stack(
               children: [
-                Flexible(
-                  flex: 3,
-                  fit: FlexFit.tight,
-                  child: FutureBuilder<void>(
-                    future: _initCameraControllerFuture,
-                    builder: (BuildContext context, AsyncSnapshot snapshot) {
-                      if (snapshot.connectionState == ConnectionState.done) {
-                        return SizedBox(
-                          width: size.width,
-                          height: size.width,
-                          child: ClipRect(
-                            child: FittedBox(
-                              fit: BoxFit.fitWidth,
-                              child: SizedBox(
-                                width: size.width,
-                                child: AspectRatio(
-                                    aspectRatio: 1 /
-                                        _cameraController!.value.aspectRatio,
-                                    child: CameraPreview(_cameraController!)),
-                              ),
-                            ),
-                          ),
-                        );
-                      } else {
-                        return Center(child: CircularProgressIndicator());
-                      }
-                    },
-                  ),
+                FutureBuilder<void>(
+                  future: _initCameraControllerFuture,
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      return SizedBox(
+                          height: double.infinity,
+                          width: double.infinity,
+                          child: CameraPreview(_cameraController!));
+                    } else {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                  },
                 ),
-                Flexible(
-                  flex: 2,
-                  child: Container(
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.symmetric(horizontal: 48.0),
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        GestureDetector(
-                          onTap: () async {
-                            try {
-                              await _cameraController!
-                                  .takePicture()
-                                  .then((value) {
-                                captureImage = File(value.path);
-                              });
+                const Positioned(
+                  left: 10,
+                  top: 20,
+                  child: GoingBackButton(),
+                ),
+                Positioned.fill(
+                  bottom: 10,
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: GestureDetector(
+                      onTap: () async {
+                        try {
+                          await _cameraController!.takePicture().then((value) {
+                            captureImage = File(value.path);
+                          });
 
-                              /// 화면 상태 변경 및 이미지 저장
-                              setState(() {
-                                isCapture = true;
-                              });
-                            } catch (e) {
-                              print("$e");
-                            }
-                          },
-                          child: Container(
-                            height: 80.0,
-                            width: 80.0,
-                            padding: const EdgeInsets.all(1.0),
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              border:
-                                  Border.all(color: Colors.black, width: 1.0),
-                              color: Colors.white,
-                            ),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                border:
-                                    Border.all(color: Colors.black, width: 3.0),
-                              ),
-                            ),
+                          /// 화면 상태 변경 및 이미지 저장
+                          setState(() {
+                            isCapture = true;
+                          });
+                        } catch (e, s) {
+                          log(e.toString(), stackTrace: s);
+                        }
+                      },
+                      child: Container(
+                          padding: const EdgeInsets.all(5),
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: ColorSystem.primary,
                           ),
-                        ),
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: IconButton(
-                            onPressed: () async {
-                              /// 후면 카메라 <-> 전면 카메라 변경
-                              cameraIndex = cameraIndex == 0 ? 1 : 0;
-                              await _initCamera();
-                            },
-                            icon: Icon(
-                              Icons.flip_camera_android,
-                              color: Colors.white,
-                              size: 34.0,
-                            ),
-                          ),
-                        ),
-                      ],
+                          child: const Icon(
+                            Icons.camera,
+                            size: 40,
+                            color: Colors.white,
+                          )),
                     ),
                   ),
                 ),
