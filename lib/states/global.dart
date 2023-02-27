457c97d243f3293da1056d68c72b72c343712500
White-Patrasche <5338095@gmail.com>
2023-02-28 01:03:31 +0900

    feat: google login and save tokens for auto login

--- lib/states/global.dart
+++ lib/states/global.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+import 'dart:io';
 import 'dart:async';
 import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart' as http;
+
 import 'package:flutter/material.dart';
+import 'package:garbage_collector/utils/utils.dart';
+import 'package:garbage_collector/env/env.dart';
 import 'package:garbage_collector/models/models.dart' as models;
 import 'package:garbage_collector/screens/screens.dart';
 import 'package:garbage_collector/styles/styles.dart';
@@ -10,6 +16,7 @@ import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class GlobalState extends GetxController {
   static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
@@ -22,18 +29,35 @@ class GlobalState extends GetxController {
 
   late GoogleMapController mapController;
 
-  Future<GoogleSignInAccount?> googleAuth() async {
+  Future<void> googleAuth() async {
     try {
       final GoogleSignInAccount? googleAccount = await GoogleSignIn().signIn();
+      if (googleAccount == null) {
+        showToast('구글 로그인 중 오류 발생했습니다.');
+        return;
+      }
+      final GoogleSignInAuthentication googleAuth =
+          await googleAccount.authentication;
 
-      return googleAccount;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final user = await models.User.googleLogin(
+        googleAccount.displayName!,
+        googleAccount.email,
+        credential.accessToken!,
+        googleAccount.photoUrl!,
+      );
+
+      login(user);
     } catch (e, s) {
       log(e.toString(), stackTrace: s);
-      return null;
     }
   }
 
-  void loadMarkers() {
+  Future<void> loadMarkers() async {
     markers['1234'] = Marker(
         markerId: const MarkerId('1234'),
         position: const LatLng(37.4950739, 126.9600609),
@@ -106,21 +130,46 @@ class GlobalState extends GetxController {
         });
   }
 
-  void login(models.User newUser) {
+  void login(models.User newUser) async {
     user.value = newUser;
+    token = newUser.accessToken!;
+    SharedPreferences.getInstance().then((pref) {
+      pref.setString('accessToken', newUser.accessToken!);
+      pref.setString('refreshToken', newUser.refreshToken!);
+    });
+  }
+
+  Future<bool> auth() async {
+    SharedPreferences.getInstance().then((pref) async {
+      try {
+        final accessToken = pref.getString('accessToken');
+        final refreshToken = pref.getString('refreshToken');
+        final user =
+            await models.User.auth(accessToken ?? '', refreshToken ?? '');
+
+        login(user);
+      } catch (e, s) {
+        log(e.toString(), stackTrace: s);
+      }
+    });
+    if (user.value == null) {
+      return false;
+    }
+    return true;
   }
 
   void changeLocation(LatLng nextPos) {
     latlng = nextPos;
   }
 
-  //TODO 데이터 로딩하는데 Future를 사용하면 async await으로 변경
   Future<void> load() async {
-    loadMarkers();
+    // final result = await auth();
 
-    Timer(const Duration(seconds: 1), () {
-      Get.offAll(() => const HomeScreen());
-    });
+    // if (result) {
+    await loadMarkers();
+    // }
+
+    Get.offAll(() => const HomeScreen());
     final location = await Location.instance.getLocation();
     if (location.latitude != null && location.longitude != null) {
       latlng = LatLng(location.latitude!, location.longitude!);
