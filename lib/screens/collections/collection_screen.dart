b53ccdfe3b67b609fea391ed358b3f760fd32395
ChoiSangwon <fdsa200@naver.com>
2023-02-27 15:16:04 +0900

    feat : add collection box

--- lib/screens/collections/collection_screen.dart
+++ lib/screens/collections/collection_screen.dart
@@ -21,6 +21,10 @@ class _CollectionScreenState extends State<CollectionScreen> {
               flex: 7,
               child: MyInfoBox(),
             ),
+            Flexible(
+              flex: 13,
+              child: CollectionBox(),
+            ),
           ],
         ),
       ),
@@ -159,6 +163,261 @@ class _MyInfoBoxState extends State<MyInfoBox> {
           ],
         ),
       ),
+    );
+  }
+}
+
+const List<Color> collectionInnerColors = <Color>[
+  Color.fromRGBO(30, 84, 165, 1),
+  Color.fromRGBO(255, 183, 43, 1),
+  Color.fromRGBO(6, 21, 155, 1),
+  Color.fromRGBO(234, 140, 140, 1),
+  Color.fromRGBO(94, 151, 85, 1),
+];
+
+const List<Color> collectionOuterTopLeftColors = <Color>[
+  Color.fromRGBO(207, 224, 248, 1),
+  Color.fromRGBO(255, 231, 148, 1),
+  Color.fromRGBO(208, 204, 253, 1),
+  Color.fromRGBO(255, 220, 220, 1),
+  Color.fromRGBO(207, 238, 202, 1),
+];
+const List<Color> collectionOuterBottomRightColors = <Color>[
+  Color.fromRGBO(66, 130, 226, 1),
+  Color.fromRGBO(255, 231, 148, 1),
+  Color.fromRGBO(93, 79, 250, 1),
+  Color.fromRGBO(234, 140, 140, 1),
+  Color.fromRGBO(94, 151, 85, 1)
+];
+
+class CollectionBox extends StatefulWidget {
+  const CollectionBox({super.key});
+
+  @override
+  State<CollectionBox> createState() => _CollectionBoxState();
+}
+
+class _CollectionBoxState extends State<CollectionBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.only(
+          left: 30,
+          right: 30,
+        ),
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(bottom: 30),
+              child: const Text(
+                "Collection",
+                style: TextStyle(
+                  color: Color.fromRGBO(94, 151, 85, 1),
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(
+                left: 30,
+                right: 30,
+                bottom: 30,
+              ),
+              child: CarouselSlider.builder(
+                  itemCount: 5, //쓰레기 종류 갯수
+                  itemBuilder: (context, index, realIndex) {
+                    return Stack(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(
+                                15,
+                              ),
+                            ),
+                            // color: collectionInnerColors.elementAt(index),
+                            gradient: LinearGradient(
+                              begin: FractionalOffset.topLeft,
+                              end: FractionalOffset.bottomRight,
+                              colors: [
+                                collectionOuterTopLeftColors[index],
+                                // Color.fromRGBO(206, 223, 198, 1)
+                                collectionOuterBottomRightColors[index],
+                              ],
+                              stops: [0.1, 1],
+                            ),
+                          ),
+                        ),
+                        Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  bottom: 10,
+                                ),
+                                child: const Text(
+                                  "Plastic",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                              Stack(
+                                children: [
+                                  Container(
+                                    height: 230,
+                                    width: 200,
+                                    decoration: BoxDecoration(
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.grey.withOpacity(0.7),
+                                          spreadRadius: 0,
+                                          blurRadius: 5.0,
+                                          offset: const Offset(0,
+                                              5), // changes position of shadow
+                                        ),
+                                      ],
+                                      color: collectionInnerColors
+                                          .elementAt(index),
+                                      borderRadius: const BorderRadius.all(
+                                        Radius.circular(15),
+                                      ),
+                                    ),
+                                  ),
+                                  Column(
+                                    children: [
+                                      Container(
+                                        child: const Icon(
+                                          Icons.ac_unit_outlined,
+                                          size: 200,
+                                        ),
+                                      ),
+                                      Container(
+                                        child: const Text(
+                                          "LV 1   콜라버려려려러",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w500),
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  top: 15,
+                                  left: 60,
+                                ),
+                                child: Row(
+                                  children: [
+                                    Flexible(
+                                      flex: 8,
+                                      child: Container(
+                                        width: 150,
+                                        height: 7,
+                                        decoration: const BoxDecoration(
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(15)),
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                    Flexible(
+                                      flex: 2,
+                                      child: Container(
+                                        alignment: Alignment.centerRight,
+                                        child: Text(
+                                          "$index%",
+                                          style: const TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(
+                                  top: 30,
+                                ),
+                                // child: TweenAnimationBuilder(
+                                //   tween: Tween<double>(begin: 0, end: 0.5),
+                                //   duration: const Duration(milliseconds: 500),
+                                //   builder: (context, value, child) =>
+                                //       LinearProgressIndicator(
+                                //     value: value,
+                                //     backgroundColor: Colors.grey,
+                                //   ),
+                                // ),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    for (int i = 0; i < 5; i++)
+                                      if (index == i)
+                                        Container(
+                                          margin: const EdgeInsets.only(
+                                            right: 10,
+                                            left: 10,
+                                          ),
+                                          child:
+                                              CollentionBottomColor(index: i),
+                                        )
+                                      else
+                                        Container(
+                                          margin: const EdgeInsets.only(
+                                            right: 10,
+                                            left: 10,
+                                          ),
+                                          child: const Icon(
+                                            Icons.circle,
+                                            size: 15,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                  options: CarouselOptions(
+                    height: 400,
+                    viewportFraction: 1,
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CollentionBottomColor extends StatefulWidget {
+  const CollentionBottomColor({super.key, required this.index});
+
+  final int index;
+  @override
+  State<CollentionBottomColor> createState() => _CollentionBottomColorState();
+}
+
+class _CollentionBottomColorState extends State<CollentionBottomColor> {
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      Icons.circle,
+      size: 15,
+      color: collectionInnerColors.elementAt(widget.index),
     );
   }
 }
