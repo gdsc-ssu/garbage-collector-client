cd1a27e8254d10263423f63e1b8f51460642d97d
White-Patrasche <5338095@gmail.com>
2023-02-28 01:04:00 +0900

    feat: temp trash screen

--- lib/screens/maps/trash_screen.dart
+++ lib/screens/maps/trash_screen.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:garbage_collector/styles/styles.dart';
+import 'package:get/get.dart';
+import 'package:garbage_collector/states/states.dart';
+import 'package:garbage_collector/screens/screens.dart';
+import 'package:garbage_collector/consts/consts.dart';
 
 class TrashScreen extends StatefulWidget {
-  const TrashScreen({super.key});
+  final String category;
+  const TrashScreen({
+    required this.category,
+    super.key,
+  });
 
   @override
   State<TrashScreen> createState() => _TrashScreenState();
 }
 
 class _TrashScreenState extends State<TrashScreen> {
+  int _index = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -15,35 +25,134 @@ class _TrashScreenState extends State<TrashScreen> {
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Text(
-          'type',
+          trashTranslate[widget.category] ?? '일반 쓰레기',
+          style: const TextStyle(color: Colors.white, fontSize: 30),
+        ),
+        const Text.rich(
+          TextSpan(
+              text: '재활용 쓰레기통',
+              style: TextStyle(color: ColorSystem.primary, fontSize: 24),
+              children: [
+                TextSpan(
+                  text: '에 버려주세요',
+                  style: TextStyle(color: Colors.white),
+                )
+              ]),
           style: TextStyle(color: Colors.white),
         ),
-        Text(
-          '재활용 쓰레기통에 버려주세요',
-          style: TextStyle(color: Colors.white),
-        ),
-        Text(
+        const SizedBox(height: 20),
+        const Text(
           '내 주변 쓰레기통',
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(color: Colors.white, fontSize: 21),
         ),
-        Container(
-          child: Text(
-            '몰루',
-            style: TextStyle(color: Colors.white),
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              _index = 1;
+            });
+          },
+          child: Container(
+            width: Get.width * 0.8,
+            height: 80,
+            padding: const EdgeInsets.all(20),
+            margin: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: (_index == 1) ? ColorSystem.primary : Colors.white,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '몰루',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '상도로',
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                    ),
+                    Text('170M'),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
-        Container(
-          child: Text(
-            '몰루',
-            style: TextStyle(color: Colors.white),
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              _index = 2;
+            });
+          },
+          child: Container(
+            width: Get.width * 0.8,
+            height: 80,
+            padding: const EdgeInsets.all(20),
+            margin: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: (_index == 2) ? ColorSystem.primary : Colors.white,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  '몰루',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '상도로',
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                    ),
+                    Text('170M'),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
-        Container(
-          child: Text(
-            '버리기',
-            style: TextStyle(color: Colors.white),
+        if (_index != 0)
+          GestureDetector(
+            onTap: () {
+              GlobalState.navigatorKey.currentState!
+                  .push(MaterialPageRoute(builder: ((context) {
+                return const MainMap();
+              })));
+            },
+            child: Container(
+              width: Get.width * 0.5,
+              height: 40,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    '길찾기',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  Icon(
+                    Icons.arrow_right_alt,
+                    color: ColorSystem.primary,
+                  ),
+                ],
+              ),
+            ),
           ),
-        )
       ],
     );
   }
