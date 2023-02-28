a4f2a6509a4ef360b8c71525fa1331e75aad04de
ChoiSangwon <fdsa200@naver.com>
2023-02-28 15:52:35 +0900

    feat : add character images

---
+++ lib/utils/character.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class TrashCharacter {
+  static Image plastic = Image.asset("assets/images/plastic.png");
+  static Image can = Image.asset("assets/images/can.png");
+  static Image general = Image.asset("assets/images/general.png");
+  static Image milk = Image.asset("assets/images/milk.png");
+  static Image glass = Image.asset("assets/images/glass.png");
+}
