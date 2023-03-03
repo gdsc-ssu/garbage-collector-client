936f236dc792e150ba4e92b69e688362d8881531
White-Patrasche <5338095@gmail.com>
2023-03-03 11:54:40 +0900

    feat: findBasket API and remodeling

--- lib/models/basket.dart
+++ lib/models/basket.dart
@@ -1,21 +1,59 @@
 import 'dart:convert';
+import 'dart:developer';
+import 'package:http/http.dart' as http;
+import 'package:garbage_collector/env/env.dart';
+import 'package:garbage_collector/utils/utils.dart';
 
 class Basket {
   final int id;
-  final String nickname;
+  final String basketName;
+  final String detailAddress;
   final double lat;
   final double lng;
+  final int? userTrash;
   final DateTime updatedAt;
 
-  Basket(this.id, this.nickname, this.lat, this.lng, this.updatedAt);
+  Basket(
+    this.id,
+    this.basketName,
+    this.detailAddress,
+    this.lat,
+    this.lng,
+    this.userTrash,
+    this.updatedAt,
+  );
 
   factory Basket.fromJson(Map<String, dynamic> json) {
     return Basket(
       json['id'] as int,
-      json['nickname'] as String,
+      json['basketName'] as String,
+      json['detailAddress'] as String,
       json['lat'] as double,
       json['lng'] as double,
+      json['userTrash'] as int?,
       DateTime.parse(json['updatedAt'] as String),
     );
+  }
+
+  static Future<List<Basket>> findBaskets(
+      String token, double lat, double lng, String type) async {
+    String api = "${ENV.apiEndpoint}/basket/recommend";
+
+    final response = await http.post(Uri.parse(api),
+        headers: {
+          "authorization": token,
+          "Content-Type": "application/json; charset=UTF-8",
+        },
+        body: jsonEncode({"lat": lat, "lng": lng, "type": type}));
+
+    if (response.statusCode == 200) {
+      List<dynamic> baskets = json.decode(response.body)['result'];
+      log(baskets.toString());
+      return baskets
+          .map((basket) => Basket.fromJson(basket as Map<String, dynamic>))
+          .toList();
+    } else {
+      throw newHTTPException(response.statusCode, response.body);
+    }
   }
 }
