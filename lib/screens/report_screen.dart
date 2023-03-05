b31d844cfe28631b51f882bfa9cf933aa6df7498
ChoiSangwon <fdsa200@naver.com>
2023-03-05 21:02:11 +0900

    feat : add background img

--- lib/screens/report_screen.dart
+++ lib/screens/report_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:garbage_collector/styles/styles.dart';
 import 'package:get/get.dart';
+import 'package:garbage_collector/models/basket.dart';
+import 'package:garbage_collector/states/global.dart';
 
 class ReportScreen extends StatefulWidget {
   const ReportScreen({super.key});
@@ -10,100 +12,144 @@ class ReportScreen extends StatefulWidget {
 }
 
 class _ReportScreenState extends State<ReportScreen> {
+  final _globalStates = Get.find<GlobalState>();
   int _index = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       backgroundColor: Colors.white,
-      body: Center(
-        child: SizedBox(
-          height: Get.height * 0.6,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                '동작구1 쓰레기통',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700,
+      body: Container(
+        decoration: const BoxDecoration(
+          color: Colors.black,
+          image: DecorationImage(
+            opacity: 0.7,
+            fit: BoxFit.fill,
+            image: AssetImage('assets/images/backgroundTrashBin.png'), // 배경 이미지
+          ),
+        ),
+        child: Center(
+          child: SizedBox(
+            height: Get.height * 0.6,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  '동작구1 쓰레기통',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-              const Text('현재 쓰레기통의 상태는 어떤가요?'),
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    _index = 1;
-                  });
-                },
-                child: Container(
-                  width: Get.width * 0.7,
-                  height: 60,
-                  decoration: BoxDecoration(
-                      color: (_index == 1) ? ColorSystem.primary : Colors.white,
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: const Offset(0, 3),
-                        ),
-                      ]),
-                  alignment: Alignment.center,
-                  child: const Text('쓰레기통의 보수가 필요해요'),
+                const Text(
+                  '현재 쓰레기통의 상태는 어떤가요?',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    _index = 2;
-                  });
-                },
-                child: Container(
-                  width: Get.width * 0.7,
-                  height: 60,
-                  decoration: BoxDecoration(
-                      color: (_index == 2) ? ColorSystem.primary : Colors.white,
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: const Offset(0, 3),
-                        ),
-                      ]),
-                  alignment: Alignment.center,
-                  child: const Text('내용물을 비워야 해요'),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _index = 1;
+                      Basket.reportBaskets(_globalStates.token, "EMPTY", 122);
+                    });
+                  },
+                  child: Container(
+                    width: Get.width * 0.7,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        color:
+                            (_index == 1) ? ColorSystem.primary : Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
+                          ),
+                        ]),
+                    alignment: Alignment.center,
+                    child: Text(
+                      '쓰레기통의 보수가 필요해요',
+                      style: TextStyle(
+                        color: (_index == 1) ? Colors.white : Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    _index = 3;
-                  });
-                },
-                child: Container(
-                  width: Get.width * 0.7,
-                  height: 60,
-                  decoration: BoxDecoration(
-                      color: (_index == 3) ? ColorSystem.primary : Colors.white,
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 5,
-                          blurRadius: 7,
-                          offset: const Offset(0, 3),
-                        ),
-                      ]),
-                  alignment: Alignment.center,
-                  child: const Text('관리자의 점검이 필요해요'),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _index = 2;
+                    });
+                  },
+                  child: Container(
+                    width: Get.width * 0.7,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        color:
+                            (_index == 2) ? ColorSystem.primary : Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
+                          ),
+                        ]),
+                    alignment: Alignment.center,
+                    child: Text(
+                      '내용물을 비워야 해요',
+                      style: TextStyle(
+                        color: (_index == 2) ? Colors.white : Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ],
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _index = 3;
+                    });
+                  },
+                  child: Container(
+                    width: Get.width * 0.7,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        color:
+                            (_index == 3) ? ColorSystem.primary : Colors.white,
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
+                          ),
+                        ]),
+                    alignment: Alignment.center,
+                    child: Text(
+                      '관리자의 점검이 필요해요',
+                      style: TextStyle(
+                        color: (_index == 3) ? Colors.white : Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
