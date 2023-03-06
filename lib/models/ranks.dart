717eefb4e715f2aa42e148192e820dc565cdc666
White-Patrasche <5338095@gmail.com>
2023-03-06 15:54:48 +0900

    feat: rank models

---
+++ lib/models/ranks.dart
@@ -0,0 +1,40 @@
+import 'package:garbage_collector/env/env.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:garbage_collector/utils/utils.dart';
+
+class Ranker {
+  final int id;
+  final String nickname;
+  final String profileImg;
+  final int totalScore;
+
+  Ranker(this.id, this.nickname, this.profileImg, this.totalScore);
+
+  factory Ranker.fromJson(Map<String, dynamic> json) {
+    return Ranker(
+      json['id'] as int,
+      json['nickname'] as String,
+      json['profileImg'] as String,
+      json['totalScore'] as int,
+    );
+  }
+
+  static Future<List<Ranker>> totalRank() async {
+    String api = "${ENV.apiEndpoint}/rank/total";
+
+    final response = await http.get(
+      Uri.parse(api),
+      headers: {"Content-Type": "application/json; charset=UTF-8"},
+    );
+
+    if (response.statusCode == 200) {
+      List<dynamic> rankers = json.decode(response.body)['result'];
+      return rankers
+          .map((ranker) => Ranker.fromJson(ranker as Map<String, dynamic>))
+          .toList();
+    } else {
+      throw newHTTPException(response.statusCode, response.body);
+    }
+  }
+}
