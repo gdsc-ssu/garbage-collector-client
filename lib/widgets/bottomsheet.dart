5b8e9b71763f6aba48aadedd629b29fd98a84dcc
White-Patrasche <5338095@gmail.com>
2023-03-11 21:23:05 +0900

    feat: merge throwable marker into normal marker

--- lib/widgets/bottomsheet.dart
+++ lib/widgets/bottomsheet.dart
@@ -9,145 +9,15 @@ import 'package:garbage_collector/states/states.dart';
 
 class MarkerBottomSheet extends StatefulWidget {
   final Basket basket;
-  const MarkerBottomSheet({required this.basket, super.key});
+  final bool isThrowable;
+  const MarkerBottomSheet(
+      {required this.basket, required this.isThrowable, super.key});
 
   @override
   State<MarkerBottomSheet> createState() => _MarkerBottomSheetState();
 }
 
 class _MarkerBottomSheetState extends State<MarkerBottomSheet> {
-  late Basket _basket;
-  double distance = -1;
-
-  @override
-  void initState() {
-    super.initState();
-    _basket = widget.basket;
-    Geolocator.getCurrentPosition().then((location) {
-      distance = Geolocator.distanceBetween(
-        location.latitude,
-        location.longitude,
-        _basket.lat,
-        _basket.lng,
-      );
-      setState(() {});
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return BottomSheet(
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(
-          top: Radius.circular(25.0),
-        ),
-      ),
-      onClosing: () {},
-      enableDrag: false,
-      builder: (context) {
-        return SizedBox(
-          height: Get.height * 0.3,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                width: 150,
-                height: 150,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                      image:
-                          Image.asset('assets/images/trash_image.png').image),
-                ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    _basket.basketName,
-                    style: const TextStyle(
-                        fontSize: 21, fontWeight: FontWeight.w700),
-                  ),
-                  const SizedBox(height: 10),
-                  if (distance == -1)
-                    const SizedBox(
-                      height: 20,
-                      width: 20,
-                      child: CircularProgressIndicator(strokeWidth: 2),
-                    )
-                  else
-                    Row(
-                      children: [
-                        const Icon(Icons.location_on),
-                        Text(
-                          '현재 위치로부터 ${distance.toInt()}m',
-                          style: const TextStyle(
-                            fontSize: 15,
-                          ),
-                        ),
-                      ],
-                    ),
-                  const SizedBox(height: 10),
-                  GestureDetector(
-                    onTap: () {
-                      Get.to(() => ReportScreen(basket: _basket));
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
-                      decoration: BoxDecoration(
-                        color: ColorSystem.primary,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey.withOpacity(0.5),
-                            spreadRadius: 2,
-                            blurRadius: 3,
-                            offset: const Offset(0, 3),
-                          ),
-                        ],
-                      ),
-                      child: Row(
-                        children: [
-                          Container(
-                            margin: const EdgeInsets.only(right: 5),
-                            child: const Icon(
-                              Icons.phone,
-                              size: 20,
-                            ),
-                          ),
-                          const Text('쓰레기통 신고하기'),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    '쓰레기통이 꽉차있거나 보수가 필요하다면 신고해주세요',
-                    style: TextStyle(fontSize: 10),
-                  ),
-                ],
-              )
-            ],
-          ),
-        );
-      },
-    );
-  }
-}
-
-class ThrowableMarkerBottomSheet extends StatefulWidget {
-  final Basket basket;
-  final bool isThrowable;
-  const ThrowableMarkerBottomSheet(
-      {required this.basket, required this.isThrowable, super.key});
-
-  @override
-  State<ThrowableMarkerBottomSheet> createState() =>
-      _ThrowableMarkerBottomSheetState();
-}
-
-class _ThrowableMarkerBottomSheetState
-    extends State<ThrowableMarkerBottomSheet> {
   final _globalStates = Get.find<GlobalState>();
   double distance = 0;
 
